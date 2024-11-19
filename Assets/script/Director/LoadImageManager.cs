c9016debfbd4e526e6297af27540bec3e10f19b0
HirokoEguchi <uni202406_02@telework.to>
2024-11-19 14:06:59 +0900

    スタートシーンの遷移画像設定

--- Assets/script/Director/LoadImageManager.cs
+++ Assets/script/Director/LoadImageManager.cs
@@ -4,12 +4,22 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class LoadImageManager : MonoBehaviour
 {
+    public TextMeshProUGUI text;
 
+    private void Start()
+    {
+        //DisplayLoadCanvas();
+        this.gameObject.SetActive(false);
+
+    }
     public void DisplayLoadCanvas()
     {
+        this.gameObject.SetActive(true);
+
         var transformCache = transform;
 
         //初期位置を保持
@@ -20,7 +30,7 @@ public class LoadImageManager : MonoBehaviour
 
 
         //移動アニメーションを開始する
-        transformCache.DOLocalMove(defaultPosition, 4.5f)
+        transformCache.DOLocalMove(defaultPosition, 1.5f)
             .SetEase(Ease.Linear)
             .OnComplete(() => { });
     }
